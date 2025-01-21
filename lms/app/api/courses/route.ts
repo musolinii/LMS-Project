52a2da2f6b56e09fe8f82b4b4b3c1ef735a9bd63
musolinii <brianbwana7265@gmail.com>
2025-01-21 11:21:24 -0500

    added  prisma schema models

---
+++ lms/app/api/courses/route.ts
@@ -0,0 +1,30 @@
+import { NextResponse } from "next/server";
+import { db } from "@/lib/db";
+import { auth } from "@clerk/nextjs/server";
+
+export async function POST(
+    req: Request,
+) {
+    try{
+        const { userId } =await auth();
+        const { title } = await req.json();
+
+        if(!userId){
+            return new NextResponse("Unauthorized",{status:401});
+        }
+
+        const course = await db.course.create({
+            data:{
+                title,
+                userId,
+            },
+        });
+
+        return NextResponse.json(course);
+
+    }catch(error){
+        console.log("[COURSES]",error);
+        return new NextResponse("Something went wrong",{status:500});
+    }
+    
+}
